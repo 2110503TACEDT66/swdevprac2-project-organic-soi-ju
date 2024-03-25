a01c80ea3c85c0dcbc4148cec4ec5b410e45f268
Gannub <gannubvv@gmail.com>
2024-03-25 14:33:21 +0700

    before pull 25/3 1.1

--- src/app/(routes)/massageshops/page.tsx
+++ src/app/(routes)/massageshops/page.tsx
@@ -1,4 +1,3 @@
-
 import getShops from "@/libs/getShops";
 import { LinearProgress } from "@mui/material";
 import { Suspense } from "react";
@@ -6,19 +5,16 @@ import ShopList from "@/components/ShopList";
 
 export default function MassageShops () {
 
-    // const shops = getShops();
+    const shops = getShops();
 
     return (
         <main className="">
-
             <div>
-                <div className="text-center my-3 font-bold text-2xl text-primary">Shops List</div>
+                <div  className="text-center my-3 font-bold text-2xl">Shops List</div>
             </div>
-            <div>
             <Suspense fallback={ <p className="font-semibold text-center text-primary my-2">LOADING ...<LinearProgress/></p>}>
-                <ShopList/>
+                <ShopList shopJson={shops}/>
             </Suspense>
-            </div>
         </main>
     );
 }
--- src/components/ShopList.tsx
+++ src/components/ShopList.tsx
@@ -1,18 +1,16 @@
 import { ShopJson } from "../../interface";
 import Link from "next/link";
 import ShopCard from "./ShopCard";
-import getShops from "@/libs/getShops";
-// {shopJson} : {shopJson: Promise<ShopJson>}
-export default async function ShopList () {
+
+export default async function ShopList ({shopJson} : {shopJson: Promise<ShopJson>}) {
     
-    // const shopJsonReady = await shopJson;
-    const shopJson = await getShops() as ShopJson;
+    const shopJsonReady = await shopJson;
 
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-center my-3 font-bold text-2xl">Shops List</h1>
             {
-                shopJson.data.map((shopItem) => (
+                shopJsonReady.data.map((shopItem) => (
                     
                     <ShopCard shop={shopItem}></ShopCard>
                 
