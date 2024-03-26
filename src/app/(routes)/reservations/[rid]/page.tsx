bc8319681e55d1f894d3e231705260e6d4c4d2ec
Jirawat Sutthisirimongkol <pufferfish.jj@gmail.com>
2024-03-26 20:58:42 +0700

    Fix some UIs

--- src/app/(routes)/reservations/[rid]/page.tsx
+++ src/app/(routes)/reservations/[rid]/page.tsx
@@ -7,6 +7,8 @@ import ShopCard from "@/components/ShopCard";
 import getShop from "@/libs/getShop";
 import CancelButton from "@/components/CancelButton";
 import Link from "next/link";
+import { decorateLink } from "@/utils/decorateLink";
+import Image from "next/image";
 
 export default async function ReservationPage({params}:{params: {rid: string}}) {
 
@@ -27,7 +29,7 @@ export default async function ReservationPage({params}:{params: {rid: string}}) 
 
     return (
         <div className="h-[90vh]">
-            <div className="w-[90vw] h-[90%] p-[5em] bg-jason m-auto mt-10 rounded-lg shadow-xl flex flex-col justify-between">
+            <div className="w-[90vw] h-[90%] p-[5em] bg-jason m-auto mt-10 rounded-lg gap-5 shadow-xl flex flex-col justify-between">
                 <div className="flex flex-col gap-5 items-center">
                     <div className="text-primary text-center text-[30px]">Reservation </div>
                     <div className="text-primary text-[20px] mt-10 text-center"> { profile.data.name} </div>
@@ -41,9 +43,18 @@ export default async function ReservationPage({params}:{params: {rid: string}}) 
                     </Link>
                     </div>
                 </div>
-                <div className="w-full  flex justify-center" >
-                    
-                        <ShopCard shop={shop.data} imgSrc={shop.data.picture? shop.data.picture : '/images/massage1.png'}/>
+                <div className="flex flex-row w-[50%] h-full p-5 m-10 ml-[25%] shadow-md rounded bg-jason text-primary hover:shadow-lg items-center justify-center transition-all" >
+                        <div className="w-[25%] h-full relative m-3">
+                            <Image src={shop.picture? decorateLink(shop.picture) : '/images/massage1.png'} alt="shop picture" 
+                            fill={true}/>
+                        </div>
+                        <div className="w-[50%] h-full relative m-3">
+                            <div>Shop: {shop.data.name}</div>
+                            <div>Address: {shop.data.address} {shop.data.district}
+                             {shop.data.province} {shop.data.postalcode}</div>
+                             <div>Tel: {shop.data.tel}</div>
+                             <div>Available: {shop.data.open_time} - {shop.data.close_time}</div>
+                        </div>
                     
                     
                 </div>
